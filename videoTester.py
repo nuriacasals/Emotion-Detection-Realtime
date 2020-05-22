aaf7eb06f16f65d6ced2475975ce66d1ca1eeb6d
nuriacasals <nuriacl28@gmail.com>
2020-05-22 11:10:42 +0200

    save emotions to csv and screenshot extreme emotions

--- videoTester.py
+++ videoTester.py
@@ -3,17 +3,28 @@ import cv2
 import numpy as np
 from keras.models import model_from_json
 from keras.preprocessing import image
+import csv
+import datetime
 
 #load model
 model = model_from_json(open("fer.json", "r").read())
 #load weights
 model.load_weights('fer.h5')
+#threshold
+emotion_thld = 0.8
 
 
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
 cap=cv2.VideoCapture(0)
+
+# Create a csv file to write the output
+now = datetime.datetime.now()
+csv_name = 'test_' + now.strftime("%Y%m%d_%H%M%S") + '.csv'
+with open(csv_name, 'w', newline='') as f:
+    thewriter = csv.writer(f)
+    thewriter.writerow(['timestamp', 'emotion'])
 
 while True:
     ret,test_img=cap.read()# captures frame and returns boolean value and captured image
@@ -40,7 +51,19 @@ while True:
         emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
         predicted_emotion = emotions[max_index]
 
+        # Save frames of extreme emotions
+        if (predictions[0][max_index] > emotion_thld) & (max_index != 6): #omit neutral emotions
+            now = datetime.datetime.now()
+            frame_name = 'frame_' + predicted_emotion + now.strftime("%Y%m%d_%H%M%S") + '.jpg'
+            cv2.imwrite(frame_name, test_img)
+
         cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+
+        with open(csv_name, 'a') as fd:
+            writer = csv.writer(fd)
+            timestamp = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+            row = [timestamp, predicted_emotion]
+            writer.writerow(row)
 
     resized_img = cv2.resize(test_img, (1000, 700))
     cv2.imshow('Facial emotion analysis ',resized_img)
